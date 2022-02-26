4aed35c829d4644c1a3235d1b8cfca56955f28e8
Gabriel Bacallado <gabriel.bacallado@bacallado.com>
2022-02-26 08:31:08 -0600

    Fix linting issues

--- tests/manifest_input_test.py
+++ tests/manifest_input_test.py
@@ -8,37 +8,37 @@ from src.aws_control_tower_manifest_builder.manifest_input import (
     ManifestInput,
 )
 
-default_region = "us-east-1"
-path_to_scp = "tests/sample_scp"
-path_to_cf = "tests/sample_templates"
+DEFAULT_REGION = "us-east-1"
+PATH_TO_SCP = "tests/sample_scp"
+PATH_TO_CF = "tests/sample_templates"
 
 error_input_data = [
     pytest.param(
-        os.path.join(path_to_scp, "scp_missing_json_error.yaml"),
+        os.path.join(PATH_TO_SCP, "scp_missing_json_error.yaml"),
         "File does not have corresponding json file",
         Scp,
         id="Tests error if SCP yaml does not have corresponding json file",
     ),
     pytest.param(
-        os.path.join(path_to_cf, "cf_template_missing_ou_accounts_error.yaml"),
+        os.path.join(PATH_TO_CF, "cf_template_missing_ou_accounts_error.yaml"),
         "Missing OU or accounts",
         CfTemplate,
         id="Tests error if file is missing OU or accounts",
     ),
     pytest.param(
-        os.path.join(path_to_cf, "cf_template_account_wrong_length_error.yaml"),
+        os.path.join(PATH_TO_CF, "cf_template_account_wrong_length_error.yaml"),
         "Account provided is not 12 digit",
         CfTemplate,
         id="Tests error if account provided is not 12 digit",
     ),
     pytest.param(
-        os.path.join(path_to_cf, "cf_template_account_not_string_error.yaml"),
+        os.path.join(PATH_TO_CF, "cf_template_account_not_string_error.yaml"),
         "Account provided is not a string",
         CfTemplate,
         id="Tests error if account provided is not a string",
     ),
     pytest.param(
-        os.path.join(path_to_cf, "empty.yaml"),
+        os.path.join(PATH_TO_CF, "empty.yaml"),
         "File does not contain the required metadata",
         CfTemplate,
         id="Tests error if input file has no required metadata",
@@ -49,13 +49,13 @@ error_input_data = [
 @pytest.mark.parametrize("file_name, error, file_type", error_input_data)
 def test_input_file_throw_error(file_name, error, file_type):
     """Test that input file throw error"""
-    new_input = file_type(file_name, default_region)
+    new_input = file_type(file_name, DEFAULT_REGION)
     assert new_input.error == error
 
 
 good_input_data = [
     pytest.param(
-        os.path.join(path_to_scp, "ec2_deny.yaml"),
+        os.path.join(PATH_TO_SCP, "ec2_deny.yaml"),
         {
             "name": "ec2_deny",
             "accounts": ["123456789012", "987456123989"],
@@ -68,7 +68,7 @@ good_input_data = [
         id="Tests that SCP yaml is proceesed correctly",
     ),
     pytest.param(
-        os.path.join(path_to_cf, "cf_template_minimal.yaml"),
+        os.path.join(PATH_TO_CF, "cf_template_minimal.yaml"),
         {
             "accounts": ["123456789012", "987456123989"],
             "name": "cf_template_minimal",
@@ -80,7 +80,7 @@ good_input_data = [
         id="Tests that input file with only required args is processed",
     ),
     pytest.param(
-        os.path.join(path_to_cf, "cf_template_detailed.yaml"),
+        os.path.join(PATH_TO_CF, "cf_template_detailed.yaml"),
         {
             "name": "detailed_template",
             "deploy_method": "stack_set",
@@ -103,7 +103,7 @@ good_input_data = [
         id="Tests that input file with all args is processed",
     ),
     pytest.param(
-        os.path.join(path_to_cf, "cf_template_local.yaml"),
+        os.path.join(PATH_TO_CF, "cf_template_local.yaml"),
         {
             "name": "templateLocalIAM",
             "description": "Template to deploy baseline IAM resources",
@@ -117,10 +117,11 @@ good_input_data = [
         id="Tests that input file local file is processed",
     ),
     pytest.param(
-        os.path.join(path_to_cf, "cf_template_S3.yaml"),
+        os.path.join(PATH_TO_CF, "cf_template_S3.yaml"),
         {
             "name": "testS3",
-            "resource_file": "https://s3.amazonaws.com/solutions-reference/customizations-for-aws-control-tower/latest/custom-control-tower-initiation.template",
+            "resource_file": "https://s3.amazonaws.com/solutions-reference/\
+customizations-for-aws-control-tower/latest/custom-control-tower-initiation.template",
             "accounts": ["123456789012", "987456123989"],
             "organizational_units": ["dev", "prod"],
             "regions": ["us-east-1", "us-east-2"],
@@ -135,7 +136,7 @@ good_input_data = [
 @pytest.mark.parametrize("file_name, error, file_type", good_input_data)
 def test_input_file(file_name, error, file_type):
     """Test that input files are processed correctly"""
-    new_input = file_type(file_name, default_region)
+    new_input = file_type(file_name, DEFAULT_REGION)
     assert new_input.metadata_dict == error
 
 
@@ -146,7 +147,7 @@ file_input_data = [
         id="Test that load_yaml throws error if file does not exists",
     ),
     pytest.param(
-        os.path.join(path_to_cf, "empty.yaml"),
+        os.path.join(PATH_TO_CF, "empty.yaml"),
         "Error, yaml is empty",
         id="Tests that load_yaml throws error if file is empty",
     ),
@@ -162,7 +163,8 @@ def test_load_yaml_file(file_name, error, caplog):
 
 def test_load_yaml_dict():
     """Test that load yaml when input is string"""
-    string_dict = "{hello : bye}"
+    dict = {"hello": "bye"}
+    string_dict = str(dict)
     dict = ManifestInput.load_yaml(string_dict, False)
     assert dict == {"hello": "bye"}
 
